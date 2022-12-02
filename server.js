6b8370049f6932928f731424a6ddb4fd2dc629d2
MorenoBell <morenobellini15@gmail.com>
2022-12-02 16:21:30 +0100

    allowed cors

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ async function connect() {
   }
 }
 connect();
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 const port = 5000;
 app.listen(port, () => console.log("server started on port" + port));
 app.use(express.json())
